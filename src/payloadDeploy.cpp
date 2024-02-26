3366f9650296ef2870650064fd6da1d64f3b9234
Ethan <ekzerba@gmail.com>
2024-02-26 16:46:31 -0700

    Add payload disconnected check for payload deployment

--- src/payloadDeploy.cpp
+++ src/payloadDeploy.cpp
@@ -12,6 +12,7 @@ const int SERVO2_PIN = 8;
 
 // Constants for payload deployment
 const int DEPLOYMENT_PIN_NUMBER = 10; 
+const int DEPLOYMENT_PAYLOAD_DISCONNECTED = 11;
 const int AIRBAG_PIN_NUMBER = 24;
 const int THRUSTER_PIN_NUMBER = 12;
 
@@ -26,6 +27,7 @@ bool payloadDeploy;
 bool airbagDeployed;
 bool thrusterKilled;
 bool thrusterDeployed;
+bool payloadDisconnected;
 
 // Function to setup payload deployment
 void setupPayloadDeploy() {
@@ -39,6 +41,7 @@ void setupPayloadDeploy() {
     Servo2.attach(SERVO2_PIN);
 
     pinMode(DEPLOYMENT_PIN_NUMBER, OUTPUT); // Set the pin as output
+    pinMode(DEPLOYMENT_PAYLOAD_DISCONNECTED, INPUT); // Set the pin as input
     pinMode(AIRBAG_PIN_NUMBER, OUTPUT); // Set the pin as output
     pinMode(THRUSTER_PIN_NUMBER, OUTPUT); // Set the pin as output
     
@@ -54,7 +57,14 @@ void deployPayload() {
         Serial5.println("PAYLOAD DEPLOYMENT ENABLED");
 
     }
-
+    if(payloadDeploy == true && !payloadDeployed){
+        if (digitalRead(DEPLOYMENT_PAYLOAD_DISCONNECTED) == LOW) {
+            payloadDisconnected = true;
+            Serial5.println("PAYLOAD DISCONNECTED");
+            Serial5.println("PAYLOAD DISCONNECTED");
+            Serial5.println("PAYLOAD DISCONNECTED");
+        }
+    }
     // Check conditions for payload deployment
     if ((altitudeAltimeter - groundLevel) >= PAYLOAD_ALTITUDE_MIN && (altitudeAltimeter - groundLevel) <= PAYLOAD_ALTITUDE_MAX && altitudeAltimeter < previousAltitude && !payloadDeployed && payloadDeploy) {
         digitalWrite(DEPLOYMENT_PIN_NUMBER, HIGH); // Set the pin to high
@@ -69,7 +79,7 @@ void deployPayload() {
 
 void deployAirbag() {
     // Check conditions for payload deployment
-    if ((altitudeAltimeter < previousAltitude) && !airbagDeployed && payloadDeployed) {
+    if ((altitudeAltimeter < previousAltitude) && !airbagDeployed && payloadDeployed && payloadDisconnected) {
         digitalWrite(AIRBAG_PIN_NUMBER, HIGH); // Set the pin to high
         airbagDeployed = true; // Set payloadDeployed to true
         Serial5.println("AIRBAG DEPLOYED");
@@ -79,7 +89,7 @@ void deployAirbag() {
 }
 
 void thrusterDeploy() {
-    if ((altitudeAltimeter - groundLevel) >= THRUSTER_ALTITUDE_MIN && (altitudeAltimeter - groundLevel) <= THRUSTER_ALTITUDE_MAX && altitudeAltimeter < previousAltitude && airbagDeployed && payloadDeployed && pitch <= -82.5 && pitch >= -97.5 && !thrusterDeployed && !thrusterKilled) {
+    if ((altitudeAltimeter - groundLevel) >= THRUSTER_ALTITUDE_MIN && (altitudeAltimeter - groundLevel) <= THRUSTER_ALTITUDE_MAX && altitudeAltimeter < previousAltitude && airbagDeployed && payloadDeployed && pitch <= -82.5 && pitch >= -97.5 && !thrusterDeployed && !thrusterKilled && payloadDisconnected) {
         digitalWrite(THRUSTER_PIN_NUMBER, HIGH); // Set the pin to high
         thrusterDeployed = true; // Set payloadDeployed to true
         Serial5.println("THRUSTER DEPLOYED");
